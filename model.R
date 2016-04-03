b4aa356089a67dfec8e1e9cc6bc8a0b668381b86
parksw3 <parksw3@mcmaster.ca>
2016-04-04 00:03:48 +0900

    fixed tabbing (2)...

--- model.R
+++ model.R
@@ -115,7 +115,7 @@ gfun <- function(parameters) {
 			
 			N <- colSums(yMat)
 			J.H <- colSums(yMat[I.HIV,] + eps_b * yMat[T.HIV,])
-		  J.S <- colSums(yMat[I.syph,])
+			J.S <- colSums(yMat[I.syph,])
 			
 			n.birth <- sweep2(StateMat(1), mu * N0)
 			n.death <- mu * yMat
