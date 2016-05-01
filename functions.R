fee7419daa9c4e6738ef9c4ac3a4c754ef14c082
parksw3 <parksw3@mcmaster.ca>
2016-05-01 12:43:19 +0900

    Added files via upload

--- functions.R
+++ functions.R
@@ -1,6 +1,5 @@
-
 as.parlist <- function(x) {
-	res <- append(x,Pars.skeleton)
+	res <- x
 	class(res) <- c("list","parlist")
 	return(res)
 }
@@ -17,6 +16,10 @@ expand.parlist <- function(x,...) {
        {
           beta.HIV = c * c_w
           beta.syph = c * beta
+          nuT_vec <- c(1, nu_t, 1)
+          nuR_mat <- matrix(rep(c(1,nu_r,1),2),3,2)
+          nuIS_mat <- matrix(rep(c(1,1,nu_is),2),3,2)
+          nuHIV_vec <- c(1, nu_HIV, nu_ARV)
        })
     return(x)
 }
